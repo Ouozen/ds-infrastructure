26b3d4ba6f9c316729c59dee0456a7d1dfcf5362
ouozen <yakunin1996@gmail.com>
2023-09-06 22:56:14 +0000

    update before_first_run

--- before_first_run.sh
+++ before_first_run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-mkdir -p dags logs plugins config postgres_data ./mlflow/minio_config
+mkdir -p dags logs plugins config postgres_data postgres_prod clickhouse_data s3_data
+
+python3 render_credentials.py
 
 docker compose up airflow-init
